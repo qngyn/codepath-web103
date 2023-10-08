342462e842835a36b2e250932837d535ef7fbe0c
Quynh Nguyen <qtn2@case.edu>
2023-10-07 21:21:36 -0400

    initial commit

---
+++ bolt_bucket/client/src/pages/EditCar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+
+const EditCar = () => {
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default EditCar
---
+++ bolt_bucket/client/src/pages/ViewCars.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+
+const ViewCars = () => {
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ViewCars
