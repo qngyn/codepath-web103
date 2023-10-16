7b724eff8c4f834c8ebf3725a6600f37020b7347
Quynh Nguyen <qtn2@case.edu>
2023-10-16 00:08:37 -0400

    lab 5

---
+++ onthefly/server/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import tripRoutes from './routes/trips.js'
+import activityRoutes from './routes/activities.js'
+import destinationRoutes from './routes/destinations.js'
+import tripDestinationRoutes from './routes/trips_destinations.js'
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">✈️ On the Fly API</h1>')
+})
+
+app.use('/api/trips', tripRoutes)
+app.use('/api/activities', activityRoutes)
+app.use('/api/destinations', destinationRoutes)
+app.use('/api/trips-destinations', tripDestinationRoutes)
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+})
